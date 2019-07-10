704fa187f80044300cce61c3a43b115588abab6f
ZoharZhou <zoharzhou@zego.im>
2019-07-10 16:18:02 +0800

    init3

--- ZegoWhiteBoardEdu/app/src/main/java/com/zego/whiteboardedu/data/ZegoDataCenter.java
+++ ZegoWhiteBoardEdu/app/src/main/java/com/zego/whiteboardedu/data/ZegoDataCenter.java
@@ -23,15 +23,10 @@ public class ZegoDataCenter {
     private static final String SP_KEY_USER_ID = "sp_key_user_id";
     private static final String SP_KEY_USER_NAME = "sp_key_user_name";
 
-    public static final long UDP_APP_ID = 1082937486L;
+    public static final long UDP_APP_ID = ;
 
 
-    public static final byte[] UDP_SIGN_DATA = new byte[]{
-            (byte) 0x60, (byte) 0x25, (byte) 0xbf, (byte) 0x4c, (byte) 0xe1, (byte) 0x10, (byte) 0x42, (byte) 0xda,
-            (byte) 0x3a, (byte) 0xa0, (byte) 0xf6, (byte) 0xab, (byte) 0xf8, (byte) 0x14, (byte) 0x9e, (byte) 0xcc,
-            (byte) 0x48, (byte) 0x8a, (byte) 0x34, (byte) 0x08, (byte) 0x45, (byte) 0xac, (byte) 0x60, (byte) 0x77,
-            (byte) 0x25, (byte) 0x1c, (byte) 0x00, (byte) 0x63, (byte) 0x4b, (byte) 0xc6, (byte) 0xd6, (byte) 0x9a
-    };
+    public static final byte[] UDP_SIGN_DATA = ;
 
     public static final ZegoUser ZEGO_USER = new ZegoUser(); // 根据自己情况初始化唯一识别USER
 
